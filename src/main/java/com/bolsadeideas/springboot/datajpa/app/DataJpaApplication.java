71420e5c10b6095740f69d8d954389f12d5fc422
JC <juanc.vivasm@gmail.com>
2023-02-02 16:42:05 -0300

    Para iniciar la carpeta de carga de imagenes y borrar su contenido. Tambien se arregla el bootstrap

--- src/main/java/com/bolsadeideas/springboot/datajpa/app/DataJpaApplication.java
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/DataJpaApplication.java
@@ -1,13 +1,23 @@
 package com.bolsadeideas.springboot.datajpa.app;
 
+import com.bolsadeideas.springboot.datajpa.app.service.UploadFileServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataJpaApplication {
+public class DataJpaApplication implements CommandLineRunner {
+	@Autowired
+	UploadFileServiceImpl uploadFileService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		uploadFileService.deleteAll();
+		uploadFileService.init();
+	}
 }
