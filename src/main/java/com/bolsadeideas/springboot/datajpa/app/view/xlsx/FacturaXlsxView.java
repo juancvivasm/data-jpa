f54eb943534dfb6715262834e1db4f11342a02e7
JC <juanc.vivasm@gmail.com>
2023-04-03 18:05:42 -0400

    Creando XLSX

---
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/view/xlsx/FacturaXlsxView.java
@@ -0,0 +1,76 @@
+package com.bolsadeideas.springboot.datajpa.app.view.xlsx;
+
+import com.bolsadeideas.springboot.datajpa.app.models.entity.Factura;
+import com.bolsadeideas.springboot.datajpa.app.models.entity.ItemFactura;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import java.util.Map;
+
+@Component("factura/ver.xlsx")
+public class FacturaXlsxView extends AbstractXlsxView {
+    @Override
+    protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Factura factura = (Factura) model.get("factura");
+        Sheet sheet = workbook
+                .createSheet("Factura Spring");
+
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(0);
+        cell.setCellValue("Datos del Cliente");
+
+        row = sheet.createRow(1);
+        cell = row.createCell(0);
+        cell.setCellValue(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
+
+        row = sheet.createRow(2);
+        cell = row.createCell(0);
+        cell.setCellValue(factura.getCliente().getEmail());
+
+        sheet.createRow(4).createCell(0).setCellValue("Datos de la factura");
+        sheet.createRow(5).createCell(0).setCellValue("Folio: " + factura.getId());
+        sheet.createRow(6).createCell(0).setCellValue("Descripción: " + factura.getDescripcion());
+        sheet.createRow(7).createCell(0).setCellValue("Fecha: " + factura.getCreateAt());
+
+        CellStyle theaderStyle = workbook.createCellStyle();
+        theaderStyle.setBorderBottom(BorderStyle.MEDIUM);
+        theaderStyle.setBorderTop(BorderStyle.MEDIUM);
+        theaderStyle.setBorderRight(BorderStyle.MEDIUM);
+        theaderStyle.setBorderLeft(BorderStyle.MEDIUM);
+        theaderStyle.setFillForegroundColor(IndexedColors.GOLD.index);
+        theaderStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        CellStyle tbodyStyle = workbook.createCellStyle();
+        tbodyStyle.setBorderBottom(BorderStyle.MEDIUM);
+        tbodyStyle.setBorderTop(BorderStyle.MEDIUM);
+        tbodyStyle.setBorderRight(BorderStyle.MEDIUM);
+        tbodyStyle.setBorderLeft(BorderStyle.MEDIUM);
+
+        Row header = sheet.createRow(9);
+        header.createCell(0).setCellValue("Producto");
+        header.createCell(1).setCellValue("Precio");
+        header.createCell(2).setCellValue("Cantidad");
+        header.createCell(3).setCellValue("Total");
+
+        header.getCell(0).setCellStyle(theaderStyle);
+        header.getCell(1).setCellStyle(theaderStyle);
+        header.getCell(2).setCellStyle(theaderStyle);
+        header.getCell(3).setCellStyle(theaderStyle);
+
+        int rownum = 10;
+        for (ItemFactura item: factura.getItems()
+             ) {
+            Row fila = sheet.createRow(rownum++);
+            fila.createCell(0).setCellValue(item.getProducto().getNombre());
+            fila.createCell(1).setCellValue(item.getProducto().getPrecio());
+            fila.createCell(2).setCellValue(item.getCantidad());
+            fila.createCell(3).setCellValue(item.calcularImporte());
+        }
+        Row filaTotal = sheet.createRow(rownum++);
+        filaTotal.createCell(2).setCellValue("TOTAL: ");
+        filaTotal.createCell(3).setCellValue(factura.getTotal());
+    }
+}
